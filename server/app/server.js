5acdda91e7f64c2cfbaf9ae5e9061bab0bdda6e0
nahidriva <nrmiss6@gmail.com>
2025-02-24 07:40:31 -0800

    edit

--- server/app/server.js
+++ server/app/server.js
@@ -39,10 +39,11 @@ class Application {
     this.#app.use(
       cors({ credentials: true, origin: process.env.ALLOW_CORS_ORIGIN })
     );
-    
+
 //added from copilot
 this.#app.use(cors(corsOptions));
 this.#app.options('*', cors(corsOptions)); 
+this.#app.use('/api', allRoutes);
 //added from copilot
 
 
